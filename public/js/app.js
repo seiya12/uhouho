dd457932cffff5111cbbca6f771b78eb5017057e
郭宇恒 <guoyuhengjp@gmail.com>
2019-07-29 23:49:47 +0900

    テスト用データを追加

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,21 @@
 import {apiAddress} from "./request/api.js";
 
-
+//テスト用API通信
 apiAddress({
+    //API通信用のアプリキーを設定する
+    //TODO::  これをAPI.jsと一緒にカプセル化したほうがいいですか？
     keyid:'9b5495f984dbf1bfaca72ae3c6036536',
+    //料理屋の名前を検索する
     name:'ぐる'
 }).then((res)=>{
-    console.log(res)
+    let itemArray = res.rest;
+    let testArae = document.getElementById('test');
+    itemArray.map((item)=>{
+        console.log(item);
+        let content = document.createElement('p');
+
+        content.innerHTML = item.name;
+
+        testArae.appendChild(content);
+    });
 });
