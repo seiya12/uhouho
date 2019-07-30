483d8ac09f16db0bb56a5b3b9739b3f7ab87ebe2
Seima-Yonesho <52072148+Seima-Yonesho@users.noreply.github.com>
2019-07-30 15:47:07 +0900

    test

---
+++ template/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>suggesh|HAL大阪|HTC|ハッカソン|teamUhouho</title>
+  <meta name="description" content="「仕事帰りや学校帰りに何か美味しいものを食べたい。だけど何を食べたいかは決まっていなくて、決めるのもめんどくさい。わかるのは人数くらい。」そんな時に「suggesh」はあなたの気分に合わせて料理を提案します。">
+  <link rel="stylesheet" href="../public/css/index.css">
+<!--  <link rel="stylesheet" href="../public/css/bootstrap.min.css">-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+  <![endif]-->
+<!--  <script src="../public/js/bootstrap.min.js"></script>-->
+</head>
+  <body>
+    <div id="wrapper">
+      <div id="header">
+        <h1>Suggesh</h1>
+        <h2>GOOD<br>EATS</h2>
+        <p>あなたの気分に合わせて料理をご提案</p>
+      </div>
+      <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel" data-pause="false" data-interval="1000">
+        <div class="carousel-inner carousel-inner--expend">
+          <div class="carousel-item active">
+            <div class="box">
+              <p>box_1</p>
+            </div>
+<!--            <img src="" class="d-block w-100" alt="">-->
+          </div>
+          <div class="carousel-item">
+            <div class="box">
+              <p>box_2</p>
+            </div>
+<!--            <img src="" class="d-block w-100" alt="">-->
+          </div>
+          <div class="carousel-item" >
+            <div class="box">
+              <p>box_3</p>
+            </div>
+<!--            <img src="" class="d-block w-100" alt="">-->
+          </div>
+        </div>
+<!--
+        <a class="carousel-control-prev carousel-control-prev--extend" href="#carouselExampleFade" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+        </a>
+-->
+      </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
