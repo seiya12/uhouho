d9a97ab8c089961dafa193de58d89727c57cb1bc
Hiroki <kondo.h.0806@gmail.com>
2019-07-30 19:34:14 +0900

    よねしょう、きみにきめた！

--- templates/result.php
+++ templates/result.php
@@ -1,12 +1,50 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>あなたへのおすすめ</title>
+    <!--  <link rel="stylesheet" href="../public/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Kosugi+Maru&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../public/css/result.css">
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+    <![endif]-->
+    <!--  <script src="../public/js/bootstrap.min.js"></script>-->
 </head>
+
 <body>
-    
+    <div class="container">
+        <div class="row mb-1 mt-4">
+            <div class="col mx-auto d-block">
+                <h1>あなたへのおすすめ</h1>
+            </div>
+        </div>
+        <div class="row mb-4">
+            <div class="col mx-auto d-block col-s-6">
+                <p>気になる料理をクリック！</p>
+            </div>
+        </div>
+        <?php foreach ($pattern[$answerNum] as $val) : ?>
+            <div class="row mb-5 mr-3 ml-3">
+                <div class="col mx-auto d-block img-fluid embed-responsive embed-responsive-16by9">
+                    <p><a href="http://list.html"><video class="embed-responsive-item" src="uhouho/videos/<?php echo $val; ?>.mp4" loop autoplay muted playsinline ></video></a></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
